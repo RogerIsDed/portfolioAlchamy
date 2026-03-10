70c524fa9ef833a382731b05d0c7e90d0b51fe5b
Oliver <Oliversto@uia.no>
2026-03-10 13:45:07 +0100

    completed challenge 2

--- portfolioAlchamy/main.mjs
+++ portfolioAlchamy/main.mjs
@@ -49,3 +49,37 @@ respons = await (await fetch(submitURL, {
 
 console.log(respons);
 
+respons = await (await fetch(statusURL, {
+    method: "GET",
+    headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": token,
+    }
+})).json();
+
+console.log(respons);
+
+respons = await (await fetch(submitURL, {
+    method: "POST",
+    headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": token,
+    },
+    body: JSON.stringify({"answer": "pi"}) 
+})).json();
+
+console.log(respons);
+
+
+respons = await (await fetch(statusURL, {
+    method: "GET",
+    headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": token,
+    }
+})).json();
+
+console.log(respons);
